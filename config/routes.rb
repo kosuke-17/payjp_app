f19e63f324dca385d7f9c8ab7c7ce661f9970c1a
kosuke-17 <tamutamu17@icloud.com>
2021-04-15 17:02:45 +0900

    クレジットカードミニアプリ実装

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'items#index'
+
+  resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
+  resources :items, only: :order do
+    post 'order', on: :member
+  end
+
+end
